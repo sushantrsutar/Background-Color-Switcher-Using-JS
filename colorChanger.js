202763ff3909551f8d58a652181c5fc7e5fda547
sushant sutar <117379840+sushantrsutar@users.noreply.github.com>
2024-12-24 19:58:27 +0530

    Add files via upload

---
+++ colorChanger.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        if (e.target.id === "grey") {
+            body.style.backgroundColor = "grey";
+        }
+        if (e.target.id === "white") {
+            body.style.backgroundColor = "white";
+        }
+        if (e.target.id === "blue") {
+            body.style.backgroundColor = "blue";
+        }
+        if (e.target.id === "yellow") {
+            body.style.backgroundColor = "yellow";
+        }
+        if(e.target.id==="red"){
+            body.style.backgroundColor="red";
+        }
+        if(e.target.id==="green"){
+            body.style.backgroundColor="green";
+        }
+    });
+});
